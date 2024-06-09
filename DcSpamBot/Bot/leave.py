e1c93a1b63abf816332660501b8034048d821ecc
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-10 01:29:52 +0530

    Update leave.py

--- DcSpamBot/Bot/leave.py
+++ DcSpamBot/Bot/leave.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram import filters, Client
 from DcSpamBot import SUDO_USER as sudo_user
 
-@Client.on_message(filters.user(sudo_user) & filters.command(["leave", "remove"], [".", "!", "/"]))
+@Client.on_message(filters.user(sudo_user) & filters.command(["leave", "remove", "gl"], [".", "!", "/"]))
 async def leave(client: Client, message: Message):
     sex  = await message.reply_text("<b>Leaving ChatID That You Provided!!</b>")
     chatid = message.command[1]
