78e3f5f33ed2b95e98fdbc73fee15d37302bf613
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-09 23:40:36 +0530

    Update __main__.py

--- DcSpamBot/__main__.py
+++ DcSpamBot/__main__.py
@@ -1,9 +1,32 @@
+
+sudo apt-get update
+sudo apt-get install ntp
+
+sudo service ntp start
+sudo ntpdate -u pool.ntp.org
+
+date
+
+
+pip install ntplib
+
+
 import asyncio
+import ntplib
+from time import ctime
 from pyrogram import idle
 from DcSpamBot import bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10, LOGGER
 
 # List of bots
 bots = [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10]
+
+def sync_time():
+    try:
+        client = ntplib.NTPClient()
+        response = client.request('pool.ntp.org')
+        print(f"System time synchronized: {ctime(response.tx_time)}")
+    except Exception as e:
+        print(f"Failed to synchronize time: {e}")
 
 async def start_bot(bot):
     try:
@@ -13,17 +36,14 @@ async def start_bot(bot):
         print(f"Failed to start bot {bot}: {e}")
 
 async def main():
-    # Start all bots concurrently
+    sync_time()
     await asyncio.gather(*(start_bot(bot) for bot in bots))
-    # Keep the program running to maintain the bot sessions
     await idle()
 
 if __name__ == "__main__":
-    # Log message
     botlogs = "Yeah Your Spam Bot Deploying Successfully, "
     team = "© BROKEN X NETWORK"
     print(botlogs + team)
 
-    # Run the main function
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
